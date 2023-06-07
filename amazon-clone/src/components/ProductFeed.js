eaecf5b59ea68094cdef70923af00de76396b0e9
Nithin-Varma <mnithin1422@gmail.com>
2023-06-07 20:52:32 +0530

    products

--- amazon-clone/src/components/ProductFeed.js
+++ amazon-clone/src/components/ProductFeed.js
@@ -3,17 +3,19 @@ import Products from './Products';
 
 function ProductFeed({ products }) {
   return (
-    <div>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
       {products.map(({ id, title, price, description, category, image }) => {
-        <Products
-          key={id}
-          id={id}
-          title = {title}
-          price = {price}
-          description = {description}
-          category = {category}
-          image = {image}
-        />
+        return(
+          <Products
+            key={id}
+            id={id}
+            title = {title}
+            price = {price}
+            description = {description}
+            category = {category}
+            image = {image}
+          />
+        )
         
       })}
 
--- amazon-clone/src/components/Products.js
+++ amazon-clone/src/components/Products.js
@@ -1,18 +1,48 @@
-import React from 'react';;
+import React, {useState} from 'react';;
 import Image from "next/image";
+import { StarIcon } from '@heroicons/react/20/solid';
+import Currency from "react-currency-formatter";
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
 function Products({ id, title, price, description, category, image }) {
+
+    const [rating] = useState(
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING +1 )) +MIN_RATING
+    );
+    const [hasPrime] = useState(Math.random() < 0.5)
+
   return (
-    <div>
-        <h1>hello boss</h1>
-        <p>{category} </p>
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
+        <p className='absolute top-2 right-2 text-xs text-gray-600'>{category} </p>
         <Image 
             src={image}
             width="200"
             height= "200"
             objectFit="contain"
         />
-      
+        <h4>{title}</h4>
+        <div className='flex'>
+            {Array(rating).fill().map((_, i) => (
+                <StarIcon className='h-5 text-yellow-500' />
+            ))}
+        </div>
+
+        <p className='text-xs my-2 line-clamp-2'>{description}</p>
+
+        <div className='mb-5'> 
+            <Currency quantity={price} currency="INR" />
+        </div>
+
+        {hasPrime && (
+            <div className='flex items-center space-x-2 -mt-5'>
+                <img className='w-12' src="https://links.papareact.com/fdw" alt="prime subscription" />
+                <p className='text-xs text-gray-500'>Free Next-Day Delivery</p>
+            </div>
+        )}
+
+        <button className='mt-auto button'>Add to Basket</button>
     </div>
   )
 }
